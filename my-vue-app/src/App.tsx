54622dcb4d3f1392ecf43017414d90074d848a87
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-05-24 12:51:40 +0200

    created and prepared project

---
+++ my-vue-app/src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+	return (
+		<p
+			style={{
+				height: "100vh",
+				background:
+					"linear-gradient( rgba(244, 165, 138, 1), rgba(237, 107, 78, 1)",
+			}}>
+			lala
+		</p>
+	);
+}
+
+export default App;

23bc4068dc901cd9a3f37b307186f8ceabafe9c9
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-05-25 19:28:56 +0200

    create home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
+import { Home } from "./pages/Home";
+
 function App() {
 	return (
-		<p
-			style={{
-				height: "100vh",
-				background:
-					"linear-gradient( rgba(244, 165, 138, 1), rgba(237, 107, 78, 1)",
-			}}>
-			lala
-		</p>
+		<>
+			<Home />
+		</>
 	);
 }
 

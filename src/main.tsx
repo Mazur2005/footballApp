928071c236fe4c1cc8de51a3c34d2a32ccc08970
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-05-25 19:29:27 +0200

    import App and global scss

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './my-vue-app/src/App.js'
+import App from './App.tsx'
+import './scss/main.scss'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>

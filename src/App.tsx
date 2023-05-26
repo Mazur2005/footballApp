0f505de3b2a1c4157462f632992ebeda9b156c36
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-05-26 21:04:32 +0200

    create router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { AuthOptions } from "./pages/AuthOptions";
+import { ErrorPage } from "./pages/ErrorPage";
 import { Home } from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+		errorElement: <ErrorPage />,
+	},
+	{
+		path: "/AuthOptions",
+		element: <AuthOptions />,
+	},
+]);
 
 function App() {
 	return (
 		<>
-			<Home />
+			<RouterProvider router={router} />
 		</>
 	);
 }

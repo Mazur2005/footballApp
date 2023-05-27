9708dbb7854534d4d0738a6a90a44937c1506b0c
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-05-27 20:37:34 +0200

    create config jest

---
+++ jest.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+	preset: "ts-jest",
+	testEnvironment: "jest-environment-jsdom",
+	transform: {
+		"^.+\\.tsx?$": "ts-jest",
+	},
+	testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	moduleNameMapper: {
+		"\\.(css|scss)$": "identity-obj-proxy",
+	},
+};

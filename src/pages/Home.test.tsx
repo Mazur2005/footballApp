369a12a031dfc879dedd66809395045bbe01a544
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-05-27 20:40:20 +0200

    create better test

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -1,23 +1,43 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { MemoryRouter, Router } from "react-router-dom";
 import { Home } from "./Home";
+import { createMemoryHistory } from "history";
+import "@testing-library/jest-dom/extend-expect";
 
-describe("Home component", () => {
-	test("renders header with correct text", () => {
-		render(<Home />);
-		const headerText = screen.getByText("Live Score");
-		expect(headerText).toBeInTheDocument();
-	});
+test("renders Home component", () => {
+	render(
+		<MemoryRouter>
+			<Home />
+		</MemoryRouter>
+	);
 
-	test("renders button with correct text and link", () => {
-		render(<Home />);
-		const button = screen.getByRole("link", { name: "TRY IT" });
-		expect(button).toBeInTheDocument();
-		expect(button.getAttribute("href")).toEqual("/AuthOptions");
-	});
+	const mainHeader = screen.getByText("Live Score");
+	expect(mainHeader).toBeInTheDocument();
 
-	test("renders image", () => {
-		render(<Home />);
-		const image = screen.getByAltText("Placeholder image");
-		expect(image).toBeInTheDocument();
+	const secondaryHeader = screen.getByText("Football score for free");
+	expect(secondaryHeader).toBeInTheDocument();
+
+	const link = screen.getByText("TRY IT");
+	expect(link).toBeInTheDocument();
+	expect(link).toHaveAttribute("href", "/AuthOptions");
+
+	const image = screen.getByLabelText("Add page");
+	expect(image).toBeInTheDocument();
+});
+test("click link and change url", async () => {
+	const history = createMemoryHistory();
+	const spy = jest.spyOn(history, "push");
+	render(
+		<Router location={history.location} navigator={history}>
+			<Home />
+		</Router>
+	);
+
+	const link = screen.getByText("TRY IT");
+	expect(history.location.pathname).toBe("/");
+	fireEvent.click(link);
+	await waitFor(() => {
+		expect(spy).toBeCalled();
+		expect(history.location.pathname).toBe("/AuthOptions");
 	});
 });

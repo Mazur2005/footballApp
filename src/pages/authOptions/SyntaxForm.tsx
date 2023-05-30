a91a63b4265834db7cc1aa9fc894ae7c30e9a999
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-05-30 15:00:18 +0200

    work with form

--- src/pages/authOptions/SyntaxForm.tsx
+++ src/pages/authOptions/SyntaxForm.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import styles from "/src/scss/module/AuthOptions/SyntaxForm.module.scss";
 
 interface Props {
 	signIn: boolean;
@@ -8,13 +9,40 @@ const logIn = ["Email", "Password"];
 
 const SyntaxForm = (props: Props) => {
 	const { signIn } = props;
+	const selectIcon = (input: string) => {
+		switch (input) {
+			case "Email":
+				return "/src/assets/icon/message.svg";
+			case "Password":
+				return "/src/assets/icon/password.svg";
+		}
+	};
 	return (
 		<div>
-			<div aria-label='cover page'></div>]
-			<form action=''>
-				<h1>Welcome</h1>
+			<div className={styles.background} aria-label='cover page'></div>
+			<form action='' className={styles.form}>
+				<h1 className={styles.form__header}>Welcome</h1>
 				{logIn.map((el, i) => (
-					<input key={i} placeholder={el} />
+					<div className={styles.form__bodyInput}>
+						<img
+							src={selectIcon(el)}
+							alt='icon'
+							className={styles.form__bodyInput__icon}
+						/>
+						<input
+							key={i}
+							placeholder={el}
+							className={styles.form__bodyInput__input}
+							type={el}
+						/>
+						{el === "Password" && (
+							<img
+								src={"/src/assets/icon/hide.svg"}
+								alt='un watch icon'
+								className={styles.form__bodyInput__icon}
+							/>
+						)}
+					</div>
 				))}
 				<div>
 					<label htmlFor='rememberMe'>

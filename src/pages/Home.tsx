8ff075803e6f3684167b3bf6f045898b2aee96de
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-06-08 17:58:38 +0200

    add local storage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,29 +1,33 @@
-import { Link } from "react-router-dom";
 import styles from "/src/scss/module/Home.module.scss";
 import { collection, getDocs } from "firebase/firestore";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { db } from "../data/fireBase";
-
 import { useNavigate } from "react-router-dom";
+import { getLocalStorage } from "../utils/localStorage";
 
 const Home = () => {
 	const navigate = useNavigate();
 	const userCollection = collection(db, "users");
 	const [isDisabled, setIsDisabled] = useState<boolean>(false);
+
 	const getUsers = async (): Promise<object | undefined> => {
-		try {
-			const users = await getDocs(userCollection);
-			const filteredData = users.docs.map(doc => ({ ...doc.data() }));
-			console.log("dupa1");
-			return filteredData;
-		} catch (error) {}
+		const users = await getDocs(userCollection);
+		const filteredData = users.docs.find(doc => {
+			if (doc.data().email === getLocalStorage("email")) {
+				return doc.data();
+			}
+		});
+
+		return filteredData?.data() !== undefined
+			? filteredData?.data()
+			: undefined;
 	};
 
 	const Link = async (): Promise<void> => {
-		// getUsers();
-		console.log("ghv");
 		setIsDisabled(true);
-		navigate("AuthOptions");
+		console.log(getUsers());
+		const isUserExist = await getUsers();
+		isUserExist !== undefined ? navigate("dupa") : navigate("AuthOptions");
 	};
 	return (
 		<>

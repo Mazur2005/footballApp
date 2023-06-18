44c4abe3cc7eb9cc595f57f15a38f402bb4dab7c
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-06-18 08:03:05 +0200

    new error message

--- src/pages/authOptions/syntax/errorMessage.ts
+++ src/pages/authOptions/syntax/errorMessage.ts
@@ -6,5 +6,9 @@ export const errorMessage = {
 	THIS_NUMBER_DOEST_EXIST: "This number doesn't exist",
 	EMAIL_IS_IN_USE: "Email is in use",
 	USER_NOT_FOUND: "User not found",
-	WRONG_PASSWORD: 'Wrong password'
+	WRONG_PASSWORD: "Wrong password",
+	MIN_NUMBER_OF_CHARACTER_IS_4: "Min number of character is 4",
+	MIN_NUMBER_OF_CHARACTER_IS_6: "Min number of character is 6",
+	MAX_NUMBER_OF_CHARACTER_IS_10: "Max number of character is 10",
+	MAX_NUMBER_OF_CHARACTER_IS_20: "Max number of character is 20",
 };

45cf4cf82affc9f316533a2e2f96ffe57b7c6c10
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-06-19 21:37:57 +0200

    add regex validation

--- src/pages/authOptions/syntax/validate.ts
+++ src/pages/authOptions/syntax/validate.ts
@@ -1,23 +1,26 @@
-import { ValidationRule } from "react-hook-form";
 import { errorMessage } from "./errorMessage";
 import { KeyboardEvent } from "react";
-import { collection, getDocs } from "firebase/firestore";
-import { db } from "../../../data/fireBase";
-
 const {
 	CANT_USE_SPECIAL__CHARACTERS,
 	USE_AT_ONE_SPECIAL__CHARACTERS,
 	USE_AT_LEAST_ONE_CAPITAL_LETTER,
 	USE_AT_LEAST_ONE_DIGIT,
 	THIS_NUMBER_DOEST_EXIST,
-	EMAIL_IS_IN_USE,
+	MIN_NUMBER_OF_CHARACTER_IS_4,
+	MIN_NUMBER_OF_CHARACTER_IS_6,
+	MAX_NUMBER_OF_CHARACTER_IS_10,
+	MAX_NUMBER_OF_CHARACTER_IS_20,
 } = errorMessage;
 
 const nameValidate = (value: string) => {
 	if (/[\W_]/g.test(value)) return CANT_USE_SPECIAL__CHARACTERS;
+	if (!/^.{4,}$/g.test(value)) return MIN_NUMBER_OF_CHARACTER_IS_4;
+	if (!/^.{1,10}$/g.test(value)) return MAX_NUMBER_OF_CHARACTER_IS_10;
 	return true;
 };
 const passwordValidate = (value: string) => {
+	if (!/^.{1,20}$/g.test(value)) return MAX_NUMBER_OF_CHARACTER_IS_20;
+	if (!/^.{6,}$/g.test(value)) return MIN_NUMBER_OF_CHARACTER_IS_6;
 	if (!/[A-Z]/g.test(value)) return USE_AT_LEAST_ONE_CAPITAL_LETTER;
 	if (!/[\W]/.test(value)) return USE_AT_ONE_SPECIAL__CHARACTERS;
 	if (!/\d/.test(value)) return USE_AT_LEAST_ONE_DIGIT;
@@ -28,14 +31,7 @@ const phoneValidate = (value: string) => {
 	return true;
 };
 
-const syntaxCheckLength = (number: number, range: string) => {
-	return {
-		value: number,
-		message: `${range} "number of characters is" ${number}`,
-	};
-};
-
-export const validation =
+export const regexValidation =
 	(el: string) =>
 	(value: string): string | boolean | undefined => {
 		switch (el) {
@@ -44,32 +40,9 @@ export const validation =
 			case "password":
 				return passwordValidate(value);
 			case "phone":
-				return phoneValidate(value);	
+				return phoneValidate(value);
 		}
 	};
 
-export const minLength = (el: string): ValidationRule<number> | undefined => {
-	switch (el) {
-		case "name":
-			return syntaxCheckLength(4, "Minimum");
-		case "password":
-			return syntaxCheckLength(6, "Minimum");
-	}
-};
-export const maxLength = (el: string): ValidationRule<number> | undefined => {
-	switch (el) {
-		case "name":
-			return syntaxCheckLength(10, "Maximum");
-		case "password":
-			return syntaxCheckLength(20, "Maximum");
-	}
-};
-
-export const preventInputE = (
-	el: string,
-	e: KeyboardEvent<HTMLInputElement>
-) => {
-	if (el === "phone") {
-		["e", "E", "+", "-"].includes(e.key) && e.preventDefault();
-	}
-};
+export const preventDefaultInput = (e: KeyboardEvent<HTMLInputElement>) =>
+	["e", "E", "+", "-"].includes(e.key) && e.preventDefault();

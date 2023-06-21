e8d7f9d542e278f4658c5107452f1a9e586b4f8b
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-06-21 17:08:05 +0200

    popup component

---
+++ src/pages/authOptions/syntax/Popup.tsx
@@ -0,0 +1,36 @@
+import { Link } from "react-router-dom";
+import styles from "/src/scss/module/Popup.module.scss";
+import { useEffect, useState } from "react";
+
+interface PopupData {
+	dataName: string;
+	header: string;
+	text: string;
+	link: string;
+	path: string;
+}
+interface Props {
+	popupData: PopupData;
+}
+
+const Popup = ({
+	popupData: { dataName, header, text, link, path },
+}: Props) => {
+	return (
+		<div className={styles.container}>
+			<h2
+				className={`${styles.container__header} ${
+					dataName === "toManyRequest" && styles.colorRed
+				}`}>
+				{header}
+			</h2>
+			<p className={styles.container__paragraph}>{text}</p>
+			<Link className={styles.container__link} to={path}>
+				{dataName === "remindPassword" && `(☞ﾟヮﾟ)☞ ${link}`}
+				{dataName === "toManyRequest" && `＞﹏＜ ${link}`}
+			</Link>
+		</div>
+	);
+};
+
+export { Popup };

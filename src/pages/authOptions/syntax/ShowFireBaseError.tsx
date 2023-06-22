91f1e5a4850cdde839c0cdbe8fe3f10fd5318276
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-06-22 18:00:22 +0200

    one more condition

--- src/pages/authOptions/syntax/ShowFireBaseError.tsx
+++ src/pages/authOptions/syntax/ShowFireBaseError.tsx
@@ -11,15 +11,16 @@ interface FireBaseError {
 
 interface Props {
 	fireBaseError: FireBaseError;
+	el: string;
 }
 
-const ShowFireBaseError = ({ fireBaseError }: Props) => {
-
+const ShowFireBaseError = ({ fireBaseError, el }: Props) => {
 	const getFireBaseError = (): string | undefined => {
 		const { emailInUse, userNotFound, wrongPassword } = fireBaseError;
-		if (emailInUse) return EMAIL_IS_IN_USE;
-		if (userNotFound) return USER_NOT_FOUND;
-		if (wrongPassword) return WRONG_PASSWORD;
+
+		if (emailInUse && el === "email") return EMAIL_IS_IN_USE;
+		if (userNotFound && el === "email") return USER_NOT_FOUND;
+		if (wrongPassword && el === "password") return WRONG_PASSWORD;
 		return undefined;
 	};
 	return <p className={styles.form__bodyInput__error}>{getFireBaseError()}</p>;

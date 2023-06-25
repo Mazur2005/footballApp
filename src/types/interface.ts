072660937d52eba3384b06c58c673b88b81e4ed6
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-06-25 20:16:13 +0200

    create file types

--- src/types/interface.ts
+++ src/types/interface.ts
@@ -1,3 +1,31 @@
+export interface BooleanDictionary {
+	[key: string]: boolean;
+}
+
 export interface StringDictionary {
 	[key: string]: string;
+}
+
+export interface PopupData {
+	dataName: string;
+	header: string;
+	text: string;
+	link: string;
+	path: string;
+}
+
+export interface ChangePath {
+	text: string;
+	link: string;
+	path: string;
+}
+export interface Structure {
+	inputs: string[];
+	header: string;
+	isLogIn: boolean;
+	isValidate: boolean;
+	isRemindPassword: boolean;
+	textOnButton: string;
+	changePath: ChangePath;
+	popupData?: PopupData;
 }

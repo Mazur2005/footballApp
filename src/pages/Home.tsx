68a31658b50dc46b8096981c82fe7c1a3ffd2451
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-07-02 20:43:54 +0200

    add upload all users

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,29 @@ import styles from "/src/scss/module/Home.module.scss";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { getLocalStorage } from "../utils/localStorage";
-import { allUsers, getUsers } from "../utils/allUsersInDataBase";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../data/fireBase";
+import { BREAK_POINT } from "../utils/breakpoint";
+
+interface AllUsers {
+	email?: string;
+	name?: string;
+	password?: string;
+	phone?: number;
+}
 
 const Home = () => {
 	const navigate = useNavigate();
-	const [isDisabled, setIsDisabled] = useState<boolean>(false);
+	const [isDisabled, setIsDisabled] = useState<boolean>(true);
+	const [allUsers, setAllUsers] = useState<AllUsers[]>([{}]);
+	const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+	const getUsers = async () => {
+		const userCollection = collection(db, "users");
+		const users = await getDocs(userCollection);
+		const filteredData = users.docs.map(doc => doc.data());
+		return filteredData;
+	};
 
 	const getRememberedFlag = async (): Promise<object | undefined> => {
 		const filteredData = allUsers.find(user => {
@@ -21,32 +39,60 @@ const Home = () => {
 	const handleLink = async (): Promise<void> => {
 		setIsDisabled(true);
 		const isUserExist = await getRememberedFlag();
-		// isUserExist !== undefined ? navigate("dupa") : navigate("AuthOptions");
-		isUserExist !== undefined ? navigate("AuthOptions") : navigate("dupa");
+		isUserExist === undefined ? navigate("AuthOptions") : navigate("dupa");
 	};
+
 	useEffect(() => {
-		setIsDisabled(true);
 		const uploadAllUsers = async () => {
-			allUsers.push(...(await getUsers()));
+			setAllUsers(await getUsers());
 			setIsDisabled(false);
 		};
 		uploadAllUsers();
 	}, []);
+
+	const handleResize = () => {
+		setWindowWidth(window.innerWidth);
+	};
+
+	useEffect(() => {
+		window.addEventListener("resize", handleResize);
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
 	return (
 		<>
-			<header className={styles.header}>
-				<h1 className={styles["header__main--header"]}>Live Score</h1>
-				<h2 className={styles["header__secondary--header"]}>
-					Football score for free
-				</h2>
-				<button
-					onClick={handleLink}
-					disabled={isDisabled}
-					className={styles.header__button}>
-					TRY IT
-				</button>
-			</header>
-			<div className={styles.img} aria-label='Add page'></div>
+			{windowWidth > BREAK_POINT ? (
+				<div className={styles.temporaryRwdError}>
+					<h1 className={styles.temporaryRwdError__header}>Oops!</h1>
+					<p className={styles.temporaryRwdError__solution}>
+						"I'm sorry, but for now we're only working on responsiveness for
+						small devices. Please open the Developer Tools and select whichever
+						phone model you prefer."
+					</p>
+				</div>
+			) : (
+				<>
+					<header className={styles.header}>
+						<h1 className={styles["header__main--header"]}>Live Score</h1>
+						<h2 className={styles["header__secondary--header"]}>
+							Football score for free
+						</h2>
+						<button
+							onClick={handleLink}
+							disabled={isDisabled}
+							className={
+								isDisabled
+									? styles["header__button--disabled"]
+									: styles.header__button
+							}>
+							TRY IT
+						</button>
+					</header>
+					<div className={styles.img} aria-label='Add page'></div>
+				</>
+			)}
 		</>
 	);
 };

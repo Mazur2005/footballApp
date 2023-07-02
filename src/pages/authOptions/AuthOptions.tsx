6e9772062c492234d274c17e109d1bd0a857fc91
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-07-02 20:44:22 +0200

    add div to better styles

--- src/pages/authOptions/AuthOptions.tsx
+++ src/pages/authOptions/AuthOptions.tsx
@@ -4,26 +4,28 @@ const AuthOptions = () => {
 	return (
 		<div className={styles.authOptions}>
 			<div className={styles.authOptions__cover} aria-label='cover page'></div>
-			<div className={styles.authOptions__boxTexts}>
-				<h1 className={styles.authOptions__boxTexts__header}>
-					Discover all about sport
-				</h1>
-				<p className={styles.authOptions__boxTexts__description}>
-					Search millions of jobs and get the inside scoop on companies. Wait
-					for what? Let’s get start it!
-				</p>
-			</div>
-			<div className={styles.authOptions__buttons}>
-				<Link
-					to={"/AuthOptions/SingIn"}
-					className={styles.authOptions__buttons__singIn}>
-					Sing in
-				</Link>
-				<Link
-					to={"/AuthOptions/SingUp"}
-					className={styles.authOptions__buttons__singUp}>
-					Sing up
-				</Link>
+			<div>
+				<div className={styles.authOptions__boxTexts}>
+					<h1 className={styles.authOptions__boxTexts__header}>
+						Discover all about sport
+					</h1>
+					<p className={styles.authOptions__boxTexts__description}>
+						Search millions of jobs and get the inside scoop on companies. Wait
+						for what? Let’s get start it!
+					</p>
+				</div>
+				<div className={styles.authOptions__buttons}>
+					<Link
+						to={"/AuthOptions/SingIn"}
+						className={styles.authOptions__buttons__singIn}>
+						Sing in
+					</Link>
+					<Link
+						to={"/AuthOptions/SingUp"}
+						className={styles.authOptions__buttons__singUp}>
+						Sing up
+					</Link>
+				</div>
 			</div>
 		</div>
 	);

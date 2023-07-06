b6af7881a3b43ccb8c402a1b532cc60bef9252a2
Mazur2005 <mazurarkadiusz2005@gmail.com>
2023-07-06 15:48:01 +0200

    config project to build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
+import path from "path";
+const isProduction = process.env.NODE_ENV === "production";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [
+		react(),
+		createSvgIconsPlugin({
+			iconDirs: [path.resolve(process.cwd(), "src/assets/icon")],
+			symbolId: "icon-[dir]-[name]",
+		}),
+	],
+	base: isProduction ? "/dist/" : "/",
+	build: {
+		assetsInlineLimit: 0,
+	},
+});
